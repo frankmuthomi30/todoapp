59ec64a16e52ee525a167b283058883a60109823
frankmuthomi30 <frankmuthomi30@gmail.com>
2023-02-06 09:13:30 +0300

    updates

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const form = document.querySelector("form");
 const taskContainer = document.getElementById("task-container");
+const completedCountDisplay = document.querySelector("#completed-count");
+const uncompletedCountDisplay = document.querySelector("#uncompleted-count");
+
+let completedCount = 0;
+let uncompletedCount = 0;
 
 form.addEventListener("submit", addTask);
 
@@ -7,38 +12,40 @@ function addTask(event) {
   event.preventDefault();
 
   const title = document.getElementById("title").value;
-  const desc = document.getElementById("DESCRIBE").value;
-  const date = document.getElementById("completedate").value;
+  const desc = document.getElementById("desc").value;
+  const date = document.getElementById("date").value;
 
   const task = `
-  <div class="task-container">
-    <h4>${title}</h4>
-    <p>${desc}</p>
-    <p>${date}</p>
-    <div class="actions">
-      <button class="complete-btn">CompLete</button>
-      <button class="delete-btn">Delete</button>
-      
+    <div class="task">
+      <h4>${title}</h4>
+      <p>${desc}</p>
+      <p>${date}</p>
+      <div class="actions">
+        <button class="complete-btn">Complete</button>
+        <button class="delete-btn">Delete</button>
+      </div>
     </div>
-  </div>
-`;
-
-
+  `;
   taskContainer.insertAdjacentHTML("beforeend", task);
-
   form.reset();
 }
 
 taskContainer.addEventListener("click", function(event) {
   const target = event.target;
-  const task = target.parentElement.parentElement;
+  const task = target.closest(".task");
 
   if (target.classList.contains("delete-btn")) {
     task.remove();
   } else if (target.classList.contains("complete-btn")) {
-    parent.classList.toggle("completed");
-  } else if (target.classList.contains("incomplete-btn")) {
-    parent.classList.toggle("completed");
+    task.classList.toggle("completed");
+    if (task.classList.contains("completed")) {
+      completedCount++;
+      uncompletedCount--;
+    } else {
+      completedCount--;
+      uncompletedCount++;
+    }
+    completedCountDisplay.textContent = completedCount;
+    uncompletedCountDisplay.textContent = uncompletedCount;
   }
 });
-task.classList.toggle("completed");
